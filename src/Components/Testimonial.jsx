328aeaefc83d4463591218272ba65a7a89ace4e3
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-09-09 14:48:45 +0545

    new

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -19,7 +19,7 @@ function Testimonial() {
   const [testimonial, setTestimonial] = useState([])
   useEffect(()=>{
     axios.get(`https://himalayanjava-server.onrender.com/customer`).then((res)=>{
-     setTestimonial(res.data.customers[0])
+     setTestimonial(res.data.customers[1])
      console.log(res.data.customers)
     })
   })
@@ -31,15 +31,18 @@ function Testimonial() {
       <div className="w-full flex flex-col md:flex-row items-center justify-between">
         <div className="relative w-full md:w-[50%] lg:w-[25%] h-full flex flex-col justify-center lg:ml-20 mb-10">
           <div className="relative w-full shadow-lg shadow-orange-200">
-            <img
+            {
+              testimonial ? <img
               src={testimonial.image}
-              alt="Loving Customer"
+              alt=""
               className="rounded-xl"
-            />
+            /> : ""
+            }
+            
           </div>
           <div className="relative">
             <h2 className="text-white text-xl font-semibold text-center mt-5 md:mt-14 mb-2">
-              {testimonial.name}
+              {testimonial.name}, {testimonial.profession}
             </h2>
             <Rating rating={testimonial.rating} className="text-3xl text-amber-500 text-center" />
           </div>
@@ -51,10 +54,7 @@ function Testimonial() {
           </div>
 
           <p className="px-5 text-sm lg:text-3xl md:text-xl font-bold">
-            The Himalayan Java Coffee house had the best coffee around Pokhara.
-            The shop is quiet, clean and has an outdoor sitting area to enjoy
-            your coffee and people watch. The staff are very friendly and very
-            helpful. The muffins here are also very good.
+            { testimonial.comment}
           </p>
           <div className="flex justify-end">
             <RiDoubleQuotesR className="text-xl md:text-5xl" />
